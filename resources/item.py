870e06c13e12a64b4d078bac84e7d80f5157d1c4
00Bhuwan <9843lalitpur@gmail.com>
2026-02-04 12:40:01 +0545

    added tag

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,5 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
 from schemas import ItemSchema, ItemUpdateSchema
