fd12f88563fc5ef2c3fa78e0b05a27092cdaab2d
00Bhuwan <9843lalitpur@gmail.com>
2026-02-09 12:05:33 +0545

    docker implemented

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.tag import blp as TagBlueprint
 
 from db import db
 
-def create_app(db_url = None):
+def create_app(db_url=None):
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -18,15 +18,14 @@ def create_app(db_url = None):
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///datanew.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///datanew.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
     api = Api(app)
-
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
